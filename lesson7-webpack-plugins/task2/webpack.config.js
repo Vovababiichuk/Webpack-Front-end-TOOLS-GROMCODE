91c41dc267fbdae8c31cec289c38607f874cd35d
Volodymyr <babihuk888@gmail.com>
2024-08-09 18:00:41 +0300

    lesson7

---
+++ lesson7-webpack-plugins/task2/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: ['./src/index.js'],
+	output: {
+		filename: 'bundle.js',
+		path: path.join(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /.(jpg|png)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+							name: '[name].[ext]',
+							outputPath: 'images',
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+	],
+}
